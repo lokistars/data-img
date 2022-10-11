f2209c83bbe59c03d347476ebafc535f58ccd405
暖阳 <532764597@qq.com>
2022-10-11 10:35:10 +0800

    选择排序

--- src/leetCode/Sort.go
+++ src/leetCode/Sort.go
@@ -9,3 +9,13 @@ func BubblingSort(arr []int32) {
 		}
 	}
 }
+
+func SelectionSort(arr []int32) {
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+}
